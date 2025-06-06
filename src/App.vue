5ed359c74ae6b65589c778bb0c8dd1eff2f04d50
Pritish229 <pritishdash29@gmail.com>
2025-06-06 13:28:47 +0530

    vue setup complete

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  
+      <h2>Hello Dada </h2>
+  
+  </header>
+
+  <RouterView />
+</template>
+
